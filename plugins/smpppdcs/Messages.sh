39a95d33945d05c9ff2d6b54062985fbc0fc9176
Pino Toscano <pino@kde.org>
2014-11-30 09:39:22 +0100

    i18n: do not extract messages from 'unittest' subdirs

--- plugins/smpppdcs/Messages.sh
+++ plugins/smpppdcs/Messages.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kopete_smpppdcs.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/unittest/'` -o $podir/kopete_smpppdcs.pot
